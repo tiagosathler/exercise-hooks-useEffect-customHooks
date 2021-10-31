f49036af7e6171a2cd5d9bcfa07714444ce3c8fc
tiagosathler <sathler@gmail.com>
2021-10-31 16:29:40 -0300

    Exerciício resolvido

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Greeting({ initialName = '' }) {
   // 🐨 inicialize o estado com o valor que vem do localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName);
-
+  const nameInLS = window.localStorage.getItem('name');
+  const initialState = nameInLS ? nameInLS : initialName ;
+  const [name, setName] = useState(initialState);
+  
   // 🐨 Utilize o hook useEffect para atualizar a
   // propriedade `name` no localStorage quando o estado for alterado
   // 💰 window.localStorage.setItem('name', name)
+  useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name]);
 
   function handleChange(event) {
     setName(event.target.value);
